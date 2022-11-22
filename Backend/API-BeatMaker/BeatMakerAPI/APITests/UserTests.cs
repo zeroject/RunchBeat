ea4621b9e5a137edb7cc35f39421fd843d8ab460
Casper Fink <cypeyt@gmail.com>
2022-11-22 17:49:36 +0100

    Made some test
    Added test if user valid and created and delted

--- Backend/API-BeatMaker/BeatMakerAPI/APITests/UserTests.cs
+++ Backend/API-BeatMaker/BeatMakerAPI/APITests/UserTests.cs
@@ -1,11 +1,14 @@
+using Application;
 using Application.Interfaces;
 using Domain;
+using Infrastructure;
 using Moq;
 
 namespace APITests
 {
     public class UserTests
     {
+        User testUser = new User(0, "DaBeaa", "gal123", "adof@gg.org", false);
         public static List<User> GetData()
         {
             var allData = new List<User>
@@ -23,18 +26,48 @@ namespace APITests
             return allData;
         }
 
-        private readonly Mock<IUserRepository<int>, I();
-
         [Theory]
-        [MemberData(nameof(GetData))]
-        public void TestIfUserIsValid(User user_)
+        [MemberData(nameof(GetData), typeof(ArgumentException))]
+        public void TestIfUserIsValid(User user_, Type expected)
         {
-
+            // Arange
+            IUserService userService;
+            userService = new UserService();
+            // Act & Assert
+            try
+            {
+                userService.CreateNewUser(user_); 
+            } catch(ArgumentException e) 
+            {
+                Assert.Equal(expected, e.GetType());
+            };
         }
-
+        [Fact]
         public void TestIfUserWasCreated()
         {
-
+            // Arange
+            IUserService userService;
+            userService = new UserService();
+            // Act
+            userService.CreateNewUser(testUser);
+            User comebackUser = userService.GetUser("DaBeaa", "gal123");
+            // Assert
+            Assert.Equal(testUser.Email, comebackUser.Email);
+        }
+        [Fact]
+        public void TestIfUserWasDeletedFromDatabase()
+        {
+            // Arange
+            IUserService userService = new UserService();
+            // Act
+            try
+            {
+                userService.DeleteUser(testUser.Id);
+                User comebackUser = userService.GetUser("DaBeaa", "gal123");
+            } catch(Exception e)
+            {
+                Assert.Equal(typeof(Exception), e.GetType());
+            }
         }
     }
 }
