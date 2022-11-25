088c555b6959049372e970c354dc178170908de8
Ominousity <abs@hotmail.dk>
2022-11-25 10:15:46 +0100

    updated beat validator

--- Backend/API-BeatMaker/BeatMakerAPI/Application/Interfaces/IBeatService.cs
+++ Backend/API-BeatMaker/BeatMakerAPI/Application/Interfaces/IBeatService.cs
@@ -6,8 +6,8 @@ namespace Application.Interfaces
     public interface IBeatService
     {
         public List<Beat> GetAllBeatsFromUser(int userId_);
-        public Beat CreateNewBeat(BeatDTO beat_);
-        public Beat UpdateBeat(BeatDTO beat_);
+        public Beat CreateNewBeat(BeatDTO beatDTO_, string userEmail_);
+        public Beat UpdateBeat(BeatDTO beatDTO_);
         public void DeleteBeat(int id_);
         public bool IsBeatStringValid(string beatstring_);
     }
--- Backend/API-BeatMaker/BeatMakerAPI/Infrastructure/BeatRepository.cs
+++ Backend/API-BeatMaker/BeatMakerAPI/Infrastructure/BeatRepository.cs
@@ -1,18 +1,34 @@
 ﻿using Application.Interfaces;
 using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure
 {
     public class BeatRepository : IBeatRepository
     {
-        public List<Beat> GetAllBeatsFromUser(int userId_)
+        private DbContextOptions<DbContext> _options;
+        public BeatRepository()
         {
-            throw new NotImplementedException();
+            _options = new DbContextOptionsBuilder<DbContext>().UseSqlite("Data Source = db.db").Options;
         }
 
-        public Beat CreateNewBeat(Beat weightEntry_)
+        public List<Beat> GetAllBeatsFromUser(int userId_)
         {
-            throw new NotImplementedException();
+            using (var context = new DbContext(_options, ServiceLifetime.Scoped))
+            {
+                return (context._beatEntries.Where(x => x.UserId == userId_).ToList() ?? throw new KeyNotFoundException("Could not find User"));
+            }
+        }
+
+        public Beat CreateNewBeat(Beat beat_)
+        {
+            using (var context = new DbContext(_options, ServiceLifetime.Transient))
+            {
+                _ = context._beatEntries.Add(beat_) ?? throw new Exception("Could not create beat in database"); ;
+                context.SaveChanges();
+                return beat_;
+            }
         }
 
         public Beat UpdateBeat(Beat weightEntry_)
