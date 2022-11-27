6a1b600e2b255cae6321f961661d892ca59a0072
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2022-11-27 14:48:03 +0100

    start work on sequencer

--- ziiqueFrontEnd/src/app/beat-maker-page/beat-maker-page.component.ts
+++ ziiqueFrontEnd/src/app/beat-maker-page/beat-maker-page.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  StepExecutor,
+  Sequencer,
+  PeriodicTicker,
+  backward, Track,
+} from "generic-step-sequencer";
+
+
 
 @Component({
   selector: 'app-beat-maker-page',
   templateUrl: './beat-maker-page.component.html',
   styleUrls: ['./beat-maker-page.component.css']
 })
+
+class MyStepExecutor implements StepExecutor<MyParameter> {
+  execute(track: Track<MyParameter>): void {
+    console.log(
+      `executing step ${track.currentStep} on ${track.parameters.trackName}`
+    );
+  }
+
+  startBeating() {
+    const sequencer = new Sequencer<MyParameter, MyStepExecutor>(
+      new MyStepExecutor()
+    );
+
+    const ticker = new PeriodicTicker(sequencer);
+
+    sequencer.addTrack({trackName: "track1"}, 16, [1,5,10,16])
+    ticker.setBpm(165);
+    ticker.start();
+  }
+}
+interface MyParameter {
+  trackName: string;
+}
 export class BeatMakerPageComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
