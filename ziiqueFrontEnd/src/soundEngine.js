6c97a44814746839423e8fcf6cca2ccfecf9f8c5
kasp441 <91524039+kasp441@users.noreply.github.com>
2022-11-30 12:45:58 +0100

    demo sounds done

--- ziiqueFrontEnd/src/soundEngine.js
+++ ziiqueFrontEnd/src/soundEngine.js
@@ -1,11 +1,13 @@
 import * as Tone from 'tone'
 import {now} from "tone";
 
-const synth = new Tone.Synth().toDestination();
-const FMS = new Tone.FMSynth(Tone.Synth).toDestination();
 const multplayer = new Tone.Players({
   urls: {
     kick: './assets/samples/Hard_Kick.mp3',
+    bass: './assets/samples/808.mp3',
+    hihat: './assets/samples/Hihat.mp3',
+    ride: './assets/samples/Ride.mp3',
+    snare: './assets/samples/Snare__Claps.mp3',
   }
 }).toDestination();
 
@@ -13,25 +15,24 @@ const multplayer = new Tone.Players({
 
 export function demoNode(x)
 {
+  console.log(multplayer.loaded);
   switch (x)
   {
     case x="A":
       multplayer.player('kick').start(now());
      break;
     case x="B":
-      FMS.triggerAttackRelease("C4", "16n");
+      multplayer.player('bass').start(now());
       break;
     case x="C":
-      FMS.triggerAttackRelease("E#4","16n");
+      multplayer.player('hihat').start(now());
       break;
     case x="D":
-      FMS.triggerAttackRelease("F#2","16n");
+      multplayer.player('ride').start(now());
       break;
     case x="E":
-      FMS.triggerAttackRelease("D8", "16n");
+      multplayer.player('snare').start(now());
       break;
-
-      console.log("is ready: " + kick.loaded)
   }
 
 }
