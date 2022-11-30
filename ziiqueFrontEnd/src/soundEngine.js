e7539496a7188c8eb53e95319e495b5903b91eaf
Nyyberg <92079143+Nyyberg@users.noreply.github.com>
2022-11-30 12:32:07 +0100

    added sample files to assets and made first demo button

--- ziiqueFrontEnd/src/soundEngine.js
+++ ziiqueFrontEnd/src/soundEngine.js
@@ -1,7 +1,14 @@
 import * as Tone from 'tone'
+import {now} from "tone";
 
 const synth = new Tone.Synth().toDestination();
 const FMS = new Tone.FMSynth(Tone.Synth).toDestination();
+const multplayer = new Tone.Players({
+  urls: {
+    kick: './assets/samples/Hard_Kick.mp3',
+  }
+}).toDestination();
+
 
 
 export function demoNode(x)
@@ -9,8 +16,8 @@ export function demoNode(x)
   switch (x)
   {
     case x="A":
-      FMS.triggerAttackRelease("G5","16n");
-      break;
+      multplayer.player('kick').start(now());
+     break;
     case x="B":
       FMS.triggerAttackRelease("C4", "16n");
       break;
@@ -23,6 +30,8 @@ export function demoNode(x)
     case x="E":
       FMS.triggerAttackRelease("D8", "16n");
       break;
+
+      console.log("is ready: " + kick.loaded)
   }
 
 }
