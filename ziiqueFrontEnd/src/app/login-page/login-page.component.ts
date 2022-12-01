10dce0b5db8559783a456410e29bb6cec4fdf8e8
Ominousity <abs@hotmail.dk>
2022-12-01 11:08:32 +0100

    added snackbar to login

--- ziiqueFrontEnd/src/app/login-page/login-page.component.ts
+++ ziiqueFrontEnd/src/app/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Routes, RouterModule} from "@angular/router";
-import {HttpService} from "../../services/http.service";
+import { HttpService } from "../../services/http.service";
+import { MatSnackBar } from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-login-page',
@@ -12,12 +13,19 @@ export class LoginPageComponent implements OnInit {
   username: any;
 
 
-  constructor(private http: HttpService) { }
+  constructor(private http: HttpService, private snackbar: MatSnackBar) { }
 
   ngOnInit(): void {
   }
 
   async Submit() {
+    if (!this.username) {
+      this.snackbar.open("Remember to enter either your email or username", "Ok")
+      
+    }
+    else if (!this.password) {
+      this.snackbar.open("Remember to enter your password", "Ok")
+    }
     let dto = {
       username: this.username,
       password: this.password
